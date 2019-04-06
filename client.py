b7cc1fcf1b34a16810722fdb5f0dc319eae91e58
Matthew Pollex <mipollex@uwaterloo.ca>
2019-04-06 17:37:16 -0400

    Add ls command

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 import mysql.connector
 
+from datetime import datetime
+
 ###################################
 ## Database connection constants ##
 ###################################
@@ -10,6 +12,30 @@ DB_CONNECTION = {
     'user':	 'root',
     'password': 'password'
 }
+
+
+class File:
+    def __init__(self, file_tuple):
+        self.ID, self.type, self.parent, self.name, self.uid, self.gid, \
+            self.size, self.perms, self.atime, self.mtime, self.ctime = file_tuple
+
+    def full_ls_format(self):
+        children = [File(tup) for tup in run_query('SELECT * FROM File WHERE parent = {}'.format(self.ID))]
+
+        if self.type == 'dir':
+            num_dir_children = len(list(child for child in children if child.type in ('dir', 'slnk', 'hlnk'))) + 2
+            file_size = len(children) + 2
+        else:
+            num_dir_children = 1
+            file_size = self.size
+
+        last_modified_time = datetime.fromtimestamp(self.mtime)
+        time_str = datetime.strftime(last_modified_time, '%b %w %H:%M:%S')
+
+        return '{}{} {} {}\t{}\t{} {} {}'.format(
+                'd' if self.type == 'dir' else '-', self.perms, num_dir_children, self.uid, self.gid,
+                file_size, time_str, self.name
+        )
 
 
 ### Runs a query on the database and returns results if any
@@ -28,7 +54,11 @@ def run_query(query):
 ### Utilities
 
 def ls(l_flag=False):
-    return ''
+    children = run_query('SELECT * FROM File WHERE parent = (SELECT * FROM PWD)')
+    children = [File(child) for child in children]
+    if l_flag:
+        return '\n'.join(child.full_ls_format() for child in children)
+    return '/t'.join(child.name for child in children)
 
 def cd(path):
     return ''
