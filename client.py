c4790238cad37e71b55f20c334efa428bc4311e6
Matthew Pollex <mipollex@uwaterloo.ca>
2019-04-06 22:00:26 -0400

    final touchups

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import mysql.connector
 
 from datetime import datetime
+from getpass import getpass
 
 ###################################
 ## Database connection constants ##
@@ -8,7 +9,7 @@ from datetime import datetime
 
 DB_CONNECTION = {
     'host': 'localhost',
-    'database': 'my_db',
+    'database': 'unix',
     'user':	 'root',
     'password': 'password'
 }
@@ -50,165 +51,137 @@ def run_query(query):
        cursor.close()
     return results or []
 
+def resolve_link(file_id):
+    while True:
+        lnk = run_query('SELECT * FROM File WHERE ID in (SELECT target FROM SoftLink WHERE source = {})'.format(file_id))
+        if not lnk:
+            raise Exception('Bad softlink')
+        lnk = File(lnk[0])
+        if lnk.type != 'slnk':
+            return lnk
+
+def validate_path(path):
+    if path.startswith('/'):
+        current_dir = 0
+    else:
+        current_dir = run_query('SELECT * FROM PWD')[0][0]
+    for name in path.split('/'):
+        if name == '..':
+            parent = run_query('SELECT parent FROM File WHERE ID = {}'.format(current_dir))
+            if not parent:
+                raise Exception('Bad path')
+            current_dir = parent[0][0]
+        elif name in ('.', ''):
+            pass
+        else:
+            chosen = run_query('SELECT * FROM File WHERE parent = {} AND name = "{}"'.format(current_dir, name))
+            if not chosen:
+                raise Exception('Bad path')
+            chosen = File(chosen[0])
+            if chosen.type == 'slnk':
+                chosen = resolve_link(chosen.ID)
+            current_dir = chosen.ID
+    return current_dir
+
+def login():
+    while True:
+        username = input('username: ')
+        password = input('password: ')
+
+        user = run_query('SELECT ID FROM User WHERE ID = "{}" AND pswd = "{}"'.format(username, password))
+        if user:
+            return user[0][0]
 
 ### Utilities
 
-def ls(l_flag=False):
-    children = run_query('SELECT * FROM File WHERE parent = (SELECT * FROM PWD)')
-    children = [File(child) for child in children]
+def ls(l_flag=False, dir_id=None, path=None):
+    if dir_id is None:
+        dir_id = run_query('SELECT * FROM PWD')[0][0]
+
+    file = File(run_query('SELECT * FROM File WHERE ID = {}'.format(dir_id))[0])
+    if path:
+        file.name = path
+    children = [file]
+    if file.type == 'dir':
+        children = run_query('SELECT * FROM File WHERE parent = {}'.format(dir_id))
+        children = [File(child) for child in children]
+
     if l_flag:
         print('\n'.join(child.full_ls_format() for child in children))
-    print('\t'.join(child.name for child in children))
+        return 0
+    print('  '.join(child.name for child in children))
+    return 0
 
-
-def pwd():
-    pwd_fileID = run_query('SELECT * FROM PWD')[0][0]
-    pwd_path = run_query('SELECT name FROM File WHERE ID=' + str(pwd_fileID))[0][0]
-    paths = get_parent_path(pwd_fileID)
-    print(pwd_path)
-
-
-def cd(destination):
-    global path_id, path_name
-
-    if destination.startswith("../"):
-        destination, path_id, path_name = trim_start_path(destination)
-        if len(destination) == 0:
-            run_query("UPDATE PWD SET DIR_ID = '" + str(path_id) + "'")
-            return
-    else:
-        path_id, path_name = find_current_dir_id()
-
-    if not destination.startswith("/"):
-        destination = "/" + destination
-
-    names = destination.split('/')
-    names = names[1:]  # If the path begins with a slash, the first child is an empty string
-
-    for i in range(len(names)):
-        find_child = run_query("SELECT ID, name from File WHERE parent = '" + str(path_id) + "' AND name = '" + names[i]
-                               + "' AND type = 'dir'")
-        if len(find_child) == 0:
-            print("Invalid Path")
-            return
-        path_id = find_child[0][0]
-    run_query("UPDATE PWD SET DIR_ID = '" + str(path_id) + "'")
-    pwd()
-    return
-
+def cd(path):
+    file_id = validate_path(path)
+    file_type = run_query('SELECT type FROM File WHERE ID = {}'.format(file_id))[0][0]
+    if file_type != 'dir':
+        raise Exception('Cannot cd to {}, not a directory'.format(path))
+    run_query('UPDATE PWD SET dir_id = {}'.format(file_id))
+    return 0
 
 def find(path, name):
-    parent_folders = path.split('/')
+    file_id = validate_path(path)
 
-    if len(parent_folders) <= 1:  # no slash in path
-        return False
-    elif len(parent_folders) >= 2:
-        if path.startswith("/"):
-            path_id, path_name = find_current_dir_id()
-            find_recursive(path, name, [path_id], [path_name])
-        elif path.startswith("../"):
-            start_path, start_path_id, start_path_name = trim_start_path(path)
-            find_recursive("/" + start_path, name, [start_path_id], [start_path_name])
-        else:
-            path_id, path_name = find_current_dir_id()
-            find_recursive("/" + path, name, [path_id], [path_name])
+    dirs = [(file_id, '' if path.startswith('/') else path)]
+    found = []
+    while dirs:
+        dir_id, dir_path = dirs.pop()
+        for tup in run_query('SELECT * FROM File WHERE parent = {}'.format(dir_id)):
+            child = File(tup)
+            # Add child to found if name matches
+            if name in child.name:
+                found.append('/'.join([dir_path, child.name]))
+            # Follow symlinks
+            if child.type == 'slnk':
+                target = resolve_link(child.ID)
+                dirs.append((target.ID, '/'.join([dir_path, child.name])))
+            elif child.type == 'dir':
+                dirs.append((child.ID, '/'.join([dir_path, child.name])))
+    
+    for path in found:
+        dir_id = validate_path(path)
+        print(path)
+        ls(True, dir_id)
+        print()
+    return 0
 
-
-### Helpers
-def get_parent_path(pwd_fileID):
-    if (pwd_fileID == 0):  # root folder
-        return ''
-    else:
-        pwd_parent_id = run_query('SELECT parent FROM File WHERE ID=' + str(pwd_fileID))[0][0]
-        pwd_parent_name = run_query('SELECT name from File WHERE ID=' + str(pwd_parent_id))[0][0]
-
-        if (pwd_parent_name != '/'):
-            return get_parent_path(pwd_parent_id) + '/' + pwd_parent_name
-        else:
-            return pwd_parent_name
-
-
-def trim_start_path(path):
-    pwd_file_id, _ = find_current_dir_id()
-
-    while path.startswith("../"):
-        parent_query = run_query("SELECT parent FROM File WHERE ID ='" + str(pwd_file_id) + "'")
-        if len(parent_query) == 0:
-            print("Invalid Path - Trim")
-            return
-
-        pwd_file_id = parent_query[0][0]
-        path = path[3:]
-    parent_name_query = run_query("SELECT name FROM File WHERE ID='" + str(pwd_file_id) + "'")
-    return path, pwd_file_id, parent_name_query[0][0]
+def pwd():
+    current_dir = File(run_query('SELECT * FROM File WHERE ID = (SELECT * FROM PWD)')[0])
+    path = current_dir.name
+    while current_dir.ID != 0:
+        current_dir = File(run_query('SELECT * FROM File WHERE ID = {}'.format(current_dir.parent))[0])
+        path = current_dir.name + '/' + path
+    return path.replace('//', '/')
 
 
-def find_recursive(path, name, parent_ids, parent_names):
-    parent_folders = path.split('/')
+## Main loop
 
-    if len(parent_folders) <= 1:  # no slashes in path
-        find_in_directory(name, parent_ids[-2], "/" + '/'.join(parent_names))
-    else:
-        response = run_query("SELECT ID, name FROM File WHERE type = 'dir' AND name = '" + parent_folders[1]
-                             + "'AND parent = " + str(parent_ids[-1]))
-
-        if len(response) <= 0:
-            print("Invalid Path")
+def command_loop():
+    cmd = input('{} {} $ '.format(user, pwd()))
+    words = cmd.split()
+    try:
+        if words[0] == 'pwd':
+            print(pwd())
+        elif words[0] == 'ls':
+            ls(len(words) == 2 and words[1] == '-l')
+        elif words[0] == 'cd':
+            cd(words[1])
+        elif words[0] == 'find':
+            find(words[1], words[2])
+        elif words[0] == 'exit':
             return False
-        else:
-            shortened_path = ""
-            if len(parent_folders) > 2:
-                shortened_path = "/".join(parent_folders.slice(1, len(parent_folders), 1))  # remove top folder
-            find_recursive(shortened_path, name, parent_ids + [response[0][0]], parent_names + [response[0][1]])
-            return True
-
-
-def find_in_directory(name, parent_id, path):
-    file_search = run_query("SELECT name FROM File WHERE name = '" + name
-                         + "' AND parent = '" + str(parent_id) + "'")
-
-    if len(file_search) > 0:
-        print(path + '/' + name)
-
-    folder_search = run_query("SELECT ID, name FROM File WHERE type = 'dir' AND parent = '" + str(parent_id) + "'")
-
-    if len(folder_search) > 0:
-        for i in range(len(folder_search)):
-            folder_id = folder_search[i][0]
-            folder_name = folder_search[i][1]
-            find_in_directory(name, folder_id, path + '/' + folder_name)
-
-
-def find_current_dir_id():
-    root_id = run_query('SELECT DIR_ID FROM PWD')[0][0]
-    name = run_query("SELECT name FROM FILE WHERE ID = '" + str(root_id) + "'")[0][0]
-    return root_id, name
+    except Exception as e:
+        print(e)
+    return True
 
 
 connection = mysql.connector.connect(**DB_CONNECTION)
 
-
 # Testing code (need to impliment a loop asking for user input)
 try:
-    path = 'home/root/.bashrc'
-    print("cd: ", cd(path))
-    print(ls(True))
-    # print(cd('home/bob/../root'))
-    print(ls(True))
-    print(find('/file/hello', 'bash'))
-    user_input = input()
-    while not user_input.startswith('exit'):
-        if user_input.startswith("pwd"):
-            pwd()
-        elif user_input.startswith("ls"):
-            ls()
-        elif user_input.startswith("cd"):
-            params = user_input.split(" ")
-            cd(params[1])
-        elif user_input.startswith("find"):
-            params = user_input.split(" ")
-            find(params[1], params[2])
-        user_input = input()
-
+    user = login()
+    while command_loop():
+        pass
 finally:
     connection.close()
