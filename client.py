3f45a1677511e13a5ecb4e11a0f2d88206352ff2
Vik Kalkat <vikramjit@clearbridgemobile.com>
2019-04-06 23:44:42 -0400

    iterative cd approach

--- client.py
+++ client.py
@@ -80,27 +80,34 @@ def get_parent_path(pwd_fileID):
 
 
 def cd(path):
+
     names = path.split('/')
-    global_children = []
+    names = names[1:] # If the path begins with a slash, the first child is an empty string
+    #/path/to/object
+
+    root_id = find_root_id()
+
     for i in range(len(names)):
-        print("name: ", names[i])
-        ID = run_query("SELECT ID FROM File WHERE name='" + name[i] + "'")[0][0]
-        if i is len(names) - 1:
+        find_child = run_query("SELECT ID, name from File WHERE parent = '" + root_id + "', name = '" + names[i] + "', type = 'dir'")
+        if len(find_child) == 0:
+            print("Invalid Path")
+        root_id = find_child[0][0]
+    run_query("UPDATE PWD SET DIR_ID = '" + root_id + "'")
+
+
+        # print("name: ", names[i])
+        # ID = run_query("SELECT ID FROM File WHERE name='" + name[i] + "'")[0][0]
+        # if i is len(names) - 1:
             
-        else:
-            child_id = run_query("SELECT ID FROM File WHERE name='" + name[i+1] + "'")[0][0]
-            print("child_id: ", child_id)
-            children = run_query('SELECT * FROM File WHERE parent =' + str(ID))
-            children = [File(child) for child in children]
-            for child in children:
-                if child.ID == child_id:
-                    print('child ID ' + child.ID + ' found')
-                    break # child in path does exist
-
-
-        
-        
-        
+        # else:
+        #     child_id = run_query("SELECT ID FROM File WHERE name='" + name[i+1] + "'")[0][0]
+        #     print("child_id: ", child_id)
+        #     children = run_query('SELECT * FROM File WHERE parent =' + str(ID))
+        #     children = [File(child) for child in children]
+        #     for child in children:
+        #         if child.ID == child_id:
+        #             print('child ID ' + child.ID + ' found')
+        #             break # child in path does exist
 
     return ''
 
@@ -115,20 +122,23 @@ def find(path, name):
             root_query = run_query("SELECT ID, name FROM File WHERE parent = NULL")
             find_recursive(path, name, [root_query[0][0]], [root_query[0][1]])
         elif path.startswith("../"):
-            pwd_file_id = find_root_id()
-            pwd_file_name = ""
-
-            while path.startswith("../"):
-                parent_query = run_query('SELECT parent FROM File WHERE ID=' + pwd_file_id)
-                if len(parent_query) == 0:
-                    print("Invalid Path")
-                    return
-
-                pwd_file_id = parent_query[0][0]
-                pwd_file_name = parent_query[0][1]
-                path = path[3:]
+            start_path = trim_start_path()
 
             find_recursive("/" + path, name, [pwd_file_id], [pwd_file_name])
+
+def trim_start_path(path):
+    pwd_file_id, pwd_name = find_current_dir_id()
+    pwd_file_name = ""
+
+    while path.startswith("../"):
+        parent_query = run_query('SELECT parent FROM File WHERE ID=' + pwd_file_id)
+        if len(parent_query) == 0:
+            print("Invalid Path")
+            return
+
+        pwd_file_id = parent_query[0][0]
+        pwd_file_name = parent_query[0][1]
+        path = path[3:]
 
 
 def find_recursive(path, name, parent_ids, parent_names):
@@ -173,8 +183,14 @@ def find_in_directory(name, parent_id, path):
 
 
 def find_root_id():
-    root_query = run_query('SELECT ID, name FROM PWD')
+    root_query = run_query('SELECT DIR_ID, name FROM PWD')
     return root_query[0][0]
+
+
+def find_current_dir_id():
+    root_query = run_query('SELECT DIR_ID FROM PWD')
+    return root_query[0][0]
+
 
 
 connection = mysql.connector.connect(**DB_CONNECTION)
